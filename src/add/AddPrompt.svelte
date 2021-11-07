fb1c923b48fd7df6375e41fabffb04e55f2186e7
LeoDog896 <LeoDog896@hotmail.com>
2021-11-07 09:57:45 -0500

    Take user input into consideration

--- src/add/AddPrompt.svelte
+++ src/add/AddPrompt.svelte
@@ -1,7 +1,12 @@
+<script lang="ts">
+	
+</script>
 <div id="prompt">
 	<input placeholder="Name"/>
 	<br/>
 	<input placeholder="Information (Optional)"/>
 	<br/>
 	<input placeholder="More Information (Optional)"/>
+	<br/>
+	<button>Done</button>
 </div>
