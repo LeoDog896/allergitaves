f0719c8bb840f80cfb31344fa362352e57b8796d
LeoDog896 <LeoDog896@hotmail.com>
2021-11-07 09:17:44 -0500

    Use fuse

--- src/searchbar/data_store.ts
+++ src/searchbar/data_store.ts
@@ -2,9 +2,14 @@ import type { Allergin } from '../data/allergins';
 import type { Readable } from 'svelte/store';
 import { writable, derived } from 'svelte/store';
 import { allergins } from '../data/allergins';
+import Fuse from 'fuse.js';
+
+const fuse = new Fuse(allergins, {
+	keys: ['name', 'types'],
+});
 
 export const searchQuery = writable('');
 export const data: Readable<Allergin[]> = derived(searchQuery, $query => {
-	const query = $query.toLowerCase();
-	return allergins.filter(allergin => allergin.name.toLowerCase().includes(query));
+	if ($query.length === 0) return allergins;
+	return fuse.search($query).map(item => item.item);
 });
